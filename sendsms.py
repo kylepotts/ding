557c472237ddb180d5bdb5f068f880992c1e664f
Tylor Garrett <tylorgarrett@gmail.com>
2015-12-02 19:50:49 -0500

    Added config file and the ability to send messages to the number in that file

--- sendsms.py
+++ sendsms.py
@@ -1,4 +1,5 @@
 from twilio.rest import TwilioRestClient
+import json
 
 account = "AC1a4978d60e81648596ecb0729fd40669"
 token = "bd14b9df21940aeefa6ba1ad5062d5cf"
@@ -6,9 +7,14 @@ client = TwilioRestClient(account, token)
 
 from_phone_num = "+13173500444"
 
+incoming = open("config.json").read();
+listOfNumbers = json.loads(incoming)
+dictionary = listOfNumbers[0];
+number = dictionary["phone_number"]
+print number
 
 def sendsms(message, to):
 	print "Sending message"
 	sms = client.messages.create(body=message, to=to, from_=from_phone_num)
 
-sendsms("This is the message", "3174903924")
+#sendsms("it fucking works", number)
